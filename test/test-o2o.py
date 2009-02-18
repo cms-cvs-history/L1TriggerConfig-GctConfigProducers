5c78b6b43d4cd9d69627efa7a0ebdcb92819d162
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-02-18 17:03:44 +0000

    more name changes

--- test/test-o2o.py
+++ test/test-o2o.py
@@ -8,7 +8,18 @@ process.MessageLogger.debugModules = cms.untracked.vstring('*')
 
 # Generate dummy L1TriggerKeyList
 process.load("CondTools.L1Trigger.L1TriggerKeyListDummy_cff")
+
+# get 
 process.load("CondTools.L1Trigger.L1TriggerKeyDummy_cff")
+process.L1TriggerKeyDummy.objectKeys = cms.VPSet()
+process.L1TriggerKeyDummy.label = cms.string('SubsystemKeysOnly')
+
+# xxxKey = csctfKey, dttfKey, rpcKey, gmtKey, rctKey, gctKey, gtKey, or tsp0Key
+process.L1TriggerKeyDummy.gctKey = cms.string('Default')
+
+# Subclass of L1ObjectKeysOnlineProdBase.
+process.load("L1TriggerConfig.GctConfigProducers.L1GctTSCObjectKeysOnline_cfi")
+process.L1GctTSCObjectKeysOnline.subsystemLabel = cms.string('')
 
 # Get configuration data from OMDS.  This is the subclass of L1ConfigOnlineProdBase.
 process.load("L1TriggerConfig.GctConfigProducers.L1GctJetFinderParamsOnline_cfi")
